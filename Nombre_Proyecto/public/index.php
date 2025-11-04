46d24a8467c6efd8eb5802bf2aa0d8c204b8096d
Ffran-g <fran.sobradiel4@gmail.com>
2025-11-04 16:29:43 +0100

    Primera Actualización

---
+++ Nombre_Proyecto/public/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto PHP</title>
+</head>
+<body>
+    <header>
+        <h1>Mi sitio Web</h1>
+        <nav>
+            <ul>
+                <li><a href="#"> Inicio </a></li>
+                <li><a href="#"> Climbing </a></li>
+                <li><a href="#"> Ferratas </a></li>
+                <li><a href="#"> Photos </a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+    </main>
+    <footer>
+         <p>&copy; 2025 Mi Sitio Web | Diseñado por <a href="#">Francisco José García Egea</a></p>
+    </footer>
+</body>
+</html>
