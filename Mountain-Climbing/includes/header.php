3beb7b7f54debe95013626ad287bff23266f5931
Ffran-g <fran.sobradiel4@gmail.com>
2025-11-05 19:33:54 +0100

    Actualizaciion

---
+++ Mountain-Climbing/includes/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mountain Climbing Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container bg-success"> 
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom"> 
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"> 
+                <svg class="bi me-2" width="40" height="32" aria-hidden="true">
+                    <use xlink:href="#bootstrap"></use>
+                </svg> 
+                <span class="fs-4 text-white">Mountain Climbing</span> 
+            </a>
+            <!-- La barra de navegación tiene que ser solo visible cuando el usuario este registrado -->
+            <ul class="nav nav-pills"> 
+                <li class="nav-item"><a href="#" class="nav-link active bg-secondary text-white" aria-current="page">Inicio</a></li> 
+                <li class="nav-item"><a href="#" class="nav-link text-white">Escalada</a></li> 
+                <li class="nav-item"><a href="#" class="nav-link text-white">Ferratas</a></li> 
+                <li class="nav-item"><a href="#" class="nav-link text-white">Rutas</a></li> 
+                <li class="nav-item"><a href="#" class="nav-link text-white">Fotos</a></li> 
+            </ul> 
+        </header> 
+    </div>
+<main>
--- Mountain-Climbing/public/index.php
+++ Mountain-Climbing/public/index.php
@@ -1,32 +1,6 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mountain Climbing Page</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container bg-success"> 
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom"> 
-            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"> 
-                <svg class="bi me-2" width="40" height="32" aria-hidden="true">
-                    <use xlink:href="#bootstrap"></use>
-                </svg> 
-                <span class="fs-4 text-white">Mountain Climbing</span> 
-            </a>
-            <!-- La barra de navegación tiene que ser solo visible cuando el usuario este registrado -->
-            <ul class="nav nav-pills"> 
-                <li class="nav-item"><a href="#" class="nav-link active bg-secondary text-white" aria-current="page">Inicio</a></li> 
-                <li class="nav-item"><a href="#" class="nav-link text-white">Escalada</a></li> 
-                <li class="nav-item"><a href="#" class="nav-link text-white">Ferratas</a></li> 
-                <li class="nav-item"><a href="#" class="nav-link text-white">Rutas</a></li> 
-                <li class="nav-item"><a href="#" class="nav-link text-white">Fotos</a></li> 
-            </ul> 
-        </header> 
-    </div>
-    
-    <main>
+<?php
+    include_once __DIR__ . "/../includes/header.php";
+?>
         <div class="container mt-5">
             <div class="card shadow-lg border-0" style="max-width: 500px; margin: auto;">
                 <div class="card-body p-4">
@@ -102,37 +76,8 @@
                 </div>
             </div>
         </div>
+    
 
-
-    </main>
-    <div class="container"> 
-        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top"> 
-            <div class="col-md-4 d-flex align-items-center"> 
-                <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1" aria-label="Bootstrap"> 
-                    <svg class="bi" width="30" height="24" aria-hidden="true">
-                        <use xlink:href="#bootstrap"></use>
-                    </svg> 
-                </a> 
-                <span class="mb-3 mb-md-0 text-body-secondary">© 2025 Mountaing Climbing, Inc</span>
-                <span class="mb-3 mb-md-0 text-body-secondary">By Francisco José García Egea</span> 
-            </div> 
-            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex"> 
-                <li class="ms-3">
-                    <a class="text-body-secondary" href="#" aria-label="Instagram">
-                        <svg class="bi" width="24" height="24" aria-hidden="true">
-                            <use xlink:href="#instagram"></use>
-                        </svg>
-                    </a>
-                </li> 
-                <li class="ms-3">
-                    <a class="text-body-secondary" href="#" aria-label="Facebook">
-                        <svg class="bi" width="24" height="24">
-                            <use xlink:href="#facebook"></use>
-                        </svg>
-                    </a>
-                </li> 
-            </ul> 
-        </footer> 
-    </div>
-</body>
-</html>
+<?php
+    include_once __DIR__ . "/../includes/footer.php";
+?>
