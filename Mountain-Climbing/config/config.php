35d5f06bc054bbe0927efb4fe370275f930b5f72
Ffran-g <fran.sobradiel4@gmail.com>
2025-11-11 14:47:30 +0100

    no idea

--- Mountain-Climbing/config/config.php
+++ Mountain-Climbing/config/config.php
@@ -2,6 +2,6 @@
 // Esta linea es para que funcione en mi ordenador en forma local
 define('BASE_URL', '/franphp/DWES_PROYECTO_PHP/Mountain-Climbing');
 
-// Descomentar esta linea es para que funcione con la configuracion basica de xampp
+// Descomentar esta linea, para que funcione con la configuracion basica de xampp
 // Juraria que esta es la ruta por defecto que usa xampp
 // define('BASE_URL', '/xampp/htdocs/Mountain-Climbing');
